34a37f6bcca99220d5bcaaac0319a227954f9c28
bilalk2907@gmail.com <bilalk2907@gmail.com>
2023-10-04 20:54:06 +0530

    Updated gitignore

--- src/ml_banking/components/data_transformation.py
+++ src/ml_banking/components/data_transformation.py
@@ -1,1 +1,30 @@
+import sys 
+import dataclasses as dataclass 
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
 
+from src.ml_banking.exception import CustomException
+from src.ml_banking.logger import logging
+import os
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
+
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+
+    def get_data_transformer_object(self):
+        try:
+            pass
+
+        except Exception as e:
+            raise CustomException(e, sys)
+            
+      
